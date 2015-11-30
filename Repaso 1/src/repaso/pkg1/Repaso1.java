65f799bd8aba2db680f1de902103e40dac3ef882
julian <julian@192.168.0.15>
2015-11-30 17:35:36 +0100

    F

---
+++ Repaso 1/src/repaso/pkg1/Repaso1.java
@@ -0,0 +1,16 @@
+package repaso.pkg1;
+
+/**
+ *
+ * @author julian
+ */
+public class Repaso1 {
+
+ 
+    public static void main(String[] args) {
+     for (int i=0;i<5;i++){
+         System.out.println("Isto é un bucle");
+     }
+    }
+    
+}
